858c5d97bdc2f9b138360b5f1e4c6cf06a5b6ec9
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-06-05 20:38:23 +0530

    added

--- p12/index.js
+++ p12/index.js
@@ -23,4 +23,22 @@ function init(){
 
 init()
 
+Array.prototype.myMap=function(cb){
+  let temp=[]
 
+  for (let i = 0; i < this.length; i++) {
+    
+     temp.push(cb(this[i],i,this))
+    
+  }
+
+  return temp
+}
+
+const nums=[1,2,3,4]
+
+let mul=nums.myMap((num,i,arr)=>{
+   return num*3
+})
+
+console.log(mul)
