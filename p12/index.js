c282a8d80a369ea26ffc59d2794fdc8ec2a9bd2c
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-06-05 20:51:26 +0530

    added

--- p12/index.js
+++ p12/index.js
@@ -42,3 +42,28 @@ let mul=nums.myMap((num,i,arr)=>{
 })
 
 console.log(mul)
+
+
+
+Array.prototype.myFilter=function (cb){
+  let temp=[]
+
+  for (let i = 0; i < this.length; i++) {
+   
+    if(cb(this[i],i,this)){
+      temp.push(this[i])
+    }
+    
+  }
+
+  return temp
+}
+
+// const nums=[1,2,3,4]
+
+
+let fil=nums.myFilter((num)=>{
+  return num>2
+})
+
+console.log(fil)
