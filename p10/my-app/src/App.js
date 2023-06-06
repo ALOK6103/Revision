e7ec4cbc7fd4bfb9a07efc304c2bf9af1cae3dc4
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-06-06 23:19:53 +0530

    added

--- p10/my-app/src/App.js
+++ p10/my-app/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import DragAndDrop from './components/DragAndDrop';
 import FileUpload from './components/FileUpload';
 import SearchBar from './components/SearchBar';
+import ChildComponent from './example1/ChildComponent';
+import ParentComponent from './example1/ParentComponent';
 
 function App() {
   return (
     <div className="App">
       {/* <DragAndDrop  /> */}
       {/* <FileUpload /> */}
-      <SearchBar />
+      {/* <ChildComponent  /> */}
+      <ParentComponent  />
+      {/* <SearchBar /> */}
     </div>
   );
 }
