8e84569dca3e5c80b05c8ddd443d7b27ea420ffd
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-06-09 10:30:04 +0530

    added

--- p13/h1.js
+++ p13/h1.js
@@ -1,15 +1,27 @@
 
-function fetchData(callback) {
-    // Simulating an asynchronous operation
-    setTimeout(function() {
-      const data = 'Hello, world!';
-      callback(data); // Invoke the callback with the data
-    }, 2000);
+// function fetchData(callback) {
+//     // Simulating an asynchronous operation
+//     setTimeout(function() {
+//       const data = 'Hello, world!';
+//       callback(data); // Invoke the callback with the data
+//     }, 2000);
+//   }
+  
+//   function processResult(result) {
+//     console.log('Received data:', result);
+//   }
+  
+//   fetchData(processResult); // Pass the processResult function as a callback
+
+function greeting(name) {
+    console.log(`Hello, ${name}`);
   }
   
-  function processResult(result) {
-    console.log('Received data:', result);
+  function processUserInput(callback) {
+    const name = "Please enter your name."
+    callback(name);
   }
   
-  fetchData(processResult); // Pass the processResult function as a callback
+  processUserInput(greeting);
+  
   
