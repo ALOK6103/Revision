59519f10250f357ae1652304100f9d2db37d9b68
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-06-17 15:37:34 +0530

    added backend

---
+++ p14/index.js
@@ -0,0 +1,52 @@
+
+// function fisrt(a){
+//     return function(b){
+//         return function(c){
+//             console.log(a,b,c)
+//         }
+//     }
+// }
+
+// fisrt(2)(3)(4)
+
+
+
+// function fisrt(a,b,c){
+   
+//             console.log(a,b,c)
+      
+// }
+
+// fisrt(2,3,4)
+
+// let a={}
+
+// a["b"]=400
+// a["c"]=500
+
+// console.log(a["b"])
+
+
+// let user={
+//     name:"alok",
+//     age:27
+// }
+
+// console.log(JSON.stringify(user))
+
+// let arr=["ab","ac","ak"]
+
+// console.log([...arr])
+
+
+let user={
+    name:"alok",
+    age:27
+}
+
+const {name,age}=user
+
+const {name:myName}=user
+
+console.log(myName,age)
+
