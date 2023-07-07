0749a03f19d331f46e50de01b48ff24e884d06d8
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-07-07 17:50:45 +0530

    Updated code

--- p21/index.js
+++ p21/index.js
@@ -61,3 +61,22 @@
 // console.log(mapped)
 
 
+// Array.prototype.myFilter=function nBc(cb){
+//     let temp=[]
+
+//     for(let i=0;i<this.length;i++){
+//         if(cb(this[i],i,this)) temp.push(this[i])
+//     }
+
+//     return temp
+// }
+
+// let arr=[4,6,8,9]
+
+// let filtered=arr.myFilter((el,i,arr)=>{
+//     return el>4
+// })
+
+// console.log(filtered)
+
+
